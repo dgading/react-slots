85d793b2690a7e88df135d290dc25e33fc32d50c
Daniel Gading <dgading@gmail.com>
2018-04-17 13:56:14 -0400

    Update Title and add firebase

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Slots</h1>
         </header>
         <CreditBoard credits={this.state.credits} />
         {this.state.gameOver &&
